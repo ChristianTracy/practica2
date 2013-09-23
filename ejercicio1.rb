051aab695bcf2b7fd3121fdf27e303f1f4197f90
ChristianTracy <christianetracy@hotmail.com>
2013-09-23 18:24:30 -0300

    Agregado comentario y problema de variable

--- ejercicio1.rb
+++ ejercicio1.rb
@@ -1,8 +1,8 @@
 puts "La suma de los múltiplos de 3 y 5 menores a 1000 es : "
-$total = 0
+total = 0
 999.times do |x|
 	if ( x.modulo(3) == 0) or (x.modulo(5) == 0)
-		$total = total+x
+		total = total+x # reduce(:+)?
 	end
 end
-puts $total
+puts total

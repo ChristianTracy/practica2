5e0459d2106fb427652c0c13cdf1477f7259a1bc
ChristianTracy <christianetracy@hotmail.com>
2013-09-24 19:48:47 -0300

    arreglado modulo

--- ejercicio1.rb
+++ ejercicio1.rb
@@ -1,7 +1,7 @@
 puts "La suma de los múltiplos de 3 y 5 menores a 1000 es : "
 total = 0
 999.times do |x|
-	if ( x.modulo(3) == 0) or (x.modulo(5) == 0)
+	if ( x % 3 == 0) or (x % 5 == 0)
 		total = total+x # reduce(:+)?
 	end
 end
